61ecb480667f59f8a291c0965c2d5bffb711c48f
bhupendra8753 <bhupendra8753@gmail.com>
2020-01-03 21:04:39 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserManagementModule } from './user-management/user-management.module';
+import { UserPlainModule } from './user-plain/user-plain.module';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+import { RouterModule } from '@angular/router';
+import { LoginComponent } from './user-management/login/login.component';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
+import { FlatpickrModule } from 'angularx-flatpickr';
+import { UserManagementService } from './user-management.service';
+import { SocketService } from './socket.service';
+import { MeetingService } from './meeting.service';
+import { UserAdminModule } from './user-admin/user-admin.module';
 
 @NgModule({
   declarations: [
@@ -10,9 +26,29 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    UserManagementModule,
+    UserPlainModule,
+    UserAdminModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    NgbModalModule,
+    FlatpickrModule.forRoot(),
+    ToastrModule.forRoot(),
+    RouterModule.forRoot([
+      { path: 'login', component: LoginComponent, pathMatch: 'full' },
+      { path: '', redirectTo: 'login', pathMatch: 'full' },
+      { path: '*', component: LoginComponent },
+      { path: '**', component: LoginComponent }
+    ]),
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory
+    })
   ],
-  providers: [],
+  providers: [UserManagementService, SocketService, MeetingService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/user-management/user-management.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ForgotPwdComponent } from './forgot-pwd/forgot-pwd.component';
+import { LoginComponent } from './login/login.component';
+import { ResetPwdComponent } from './reset-pwd/reset-pwd.component';
+import { SignupComponent } from './signup/signup.component';
+import { VerifyComponent } from './verify/verify.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ToastrModule } from 'ngx-toastr';
+import { RouterModule } from '@angular/router';
+
+
+@NgModule({
+  declarations: [ForgotPwdComponent, LoginComponent, ResetPwdComponent, SignupComponent, VerifyComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+        ReactiveFormsModule,
+        ToastrModule.forRoot(),
+        RouterModule.forChild([
+            { path: 'signup', component: SignupComponent },
+            { path: 'forgotPwd', component: ForgotPwdComponent },
+            { path: 'resetPwd', component: ResetPwdComponent },
+            { path: 'verifyUser', component: VerifyComponent },
+        ])
+    ]
+})
+export class UserManagementModule { }
